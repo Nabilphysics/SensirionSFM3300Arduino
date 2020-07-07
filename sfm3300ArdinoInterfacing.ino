11f652ee1f8afa6c5555bb8427901a48ee497e72
Nabilphysics <nabilsust@gmail.com>
2020-07-08 02:46:02 +0600

    update temp reading

--- sfm3300ArdinoInterfacing.ino
+++ sfm3300ArdinoInterfacing.ino
@@ -9,14 +9,10 @@
 */
 #include <Wire.h>
 #include "SFM3300.h"
+//#define flowWithTemperature //uncomment if you want to measure Flow with Temperature
 
 SFM3300 sensirionFlow(64);
 //Left to Right- VDD,SCL(A5),GND,SDA(A4)
-// Sensor Connection: 
-// Sensor Power > sensorPowerPin or Use This pin for transistor switcing(Trigger) to Deliver +5V
-// Data > SDA 
-// Clock > SCK
-// GND > GND
 long int sensirion3300Offset = 32768; // Offset for Sensirion 3300 Flow Sensor
 float scaleFactor = 120.0; //For Air
 uint8_t sensorPowerPin = 5; // Since Sensor only consume 5mw power we could power it from Microcontroller Pin, You may use Transistor Switching
@@ -34,25 +30,31 @@ void setup() {
 }
 
 void loop() {
-  /////////////// Flow Measure ///////////////
-
+ /////////// Flow /////////////// 
   unsigned int result = sensirionFlow.getvalue();
-  float flow = (float(result) - sensirion3300Offset) / scaleFactor;
-  Serial.print(flow);
-  
-////////////////// Temperature Measure /////////////
-  unsigned int temp = sensirionFlow.tempRead();
-  float actualTemp = (float(temp) - 20000) / 100;
-  Serial.print(" , ");
-  Serial.println(actualTemp);
-  
+  float flow = ((float(result) - sensirion3300Offset) / scaleFactor)-0.3666 ;
+  Serial.println(flow);
   delay(100);
 
-//////// Error Handling ////////////////////////////
-  if (flow < -270.00) {
-    sensirionFlow.hardReset(sensorPowerPin); //Sensor Power ON/OFF
-    sensirionFlow.init();
-    delay(1000);
+  if(flow < -270.00){
+  sensirionFlow.hardReset(sensorPowerPin);
+  delay(500); 
+  sensirionFlow.init();
+  delay(1000);
   }
+
+  
+
+/*
+////////////////// Temp /////////////
+//Uncomment #define flowWithTemperature if you active temperature measurement and flow simaltaniously
+unsigned int temp = sensirionFlow.tempRead();
+float actualTemp = (float(temp) - 20000)/100;
+  Serial.print("temp= ");
+  Serial.println(actualTemp);
+  delay(100);
+*/
+  
+  
 
 }
